88382afed24ddfe210850cbf08b07aeb98c274b7
vaishnavibhukya <150107316+vaishnavibhukya@users.noreply.github.com>
2026-07-01 22:38:53 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        int n=s1.length();
+        int m =s2.length();
+        if(n>m){
+            return false;
+        }
+        char[] ch1=s1.toCharArray();
+        Arrays.sort(ch1);
+
+        for(int i=0;i<=m-n;i++){
+
+            String str=s2.substring(i , i+n);
+            char[] ch2=str.toCharArray();
+            Arrays.sort(ch2);
+            if(Arrays.equals(ch1,ch2)){
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
+
+       
